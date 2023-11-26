b98064ba64f4b6399a1133548a75393a30ea571d
oleksadnr <kharchenkowrk@gmail.com>
2023-11-26 11:53:00 +0100

    add using Annotation\Route

--- project/src/Controller/InvitationController.php
+++ project/src/Controller/InvitationController.php
@@ -8,6 +8,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 class InvitationController extends AbstractController
 {
